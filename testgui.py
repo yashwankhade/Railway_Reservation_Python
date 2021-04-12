1d76fd47dd5f8091154d5f9c4739f4d31309c0bc
Hardik Shah <hardikshah0702@gmail.com>
2021-04-12 22:44:58 +0530

    Back Button Update

--- testgui.py
+++ testgui.py
@@ -1,3 +1,4 @@
+import sqlite3
 from tkinter import *
 import tkinter
 import tkinter as tk
@@ -8,52 +9,92 @@ import trains
 
 global date1
 global src
-root = Tk()
 
-root.geometry('500x400')
-
-def check():
-    if cmb.get()==cmb1.get():
-        messagebox.showinfo("Error","Source and Destination cannot be same")
-    else:
-        trains.trains(cmb.get(),cmb1.get())
-    date1=date.get().split('/')
-    print(date1)
-    if int(date1[0])>31:
-        messagebox.showinfo("","Invalid date")
-    root.destroy()
+conn = sqlite3.connect('trains.db')
+c = conn.cursor()
 
 
+def enter_train_details():
+    root = Tk()
+    root.geometry('500x400')
 
-#Heading
-Heading=Label(root, text="Railway Reservation System", font=30, fg='pink', bg='green', justify='center')
-Heading.grid(row=0,column=3)
+    def check():
+        if cmb.get() == cmb1.get():
+            messagebox.showinfo(
+                "Error", "Source and Destination cannot be same")
+        else:
+            trains10(cmb.get(), cmb1.get())
+        date1 = date.get().split('/')
+        #print(date1)
+        if int(date1[0]) > 31:
+            messagebox.showinfo("", "Invalid date")
+        root.destroy()
 
-#Source
-source = Label(root, text='Source')
-source.grid(row=1,column=1)
-#Combobox-Source
-cmb = ttk.Combobox(root, width="10", values=("Pune","Mumbai","Delhi","Chennai"))
-cmb.grid(row=1, column=2)
+    # Heading
+    Heading = Label(root, text="Railway Reservation System",
+                    font=30, fg='pink', bg='green', justify='center')
+    Heading.grid(row=0, column=3)
 
-#Destination
-dest = Label(root, text='Destination')
-dest.grid(row=2,column=1)
-#Combobox-Dest
-cmb1 = ttk.Combobox(root, width="10", values=("Pune","Mumbai","Delhi","Chennai"))
-cmb1.grid(row=2, column=2)
+    # Source
+    source = Label(root, text='Source')
+    source.grid(row=1, column=1)
+    # Combobox-Source
+    cmb = ttk.Combobox(root, width="10", values=(
+        "Pune", "Mumbai", "Delhi", "Chennai"))
+    cmb.grid(row=1, column=2)
 
-#Date
-date_label=Label(root, text="Date")
-date_label.grid(row=3,column=1)
-date = Entry(root)
-date.grid(row=3,column=2)
-#Submit Button
-submit= Button(root, text='Show Trains', command=check)
-submit.grid(row=4,column=1)
+    # Destination
+    dest = Label(root, text='Destination')
+    dest.grid(row=2, column=1)
+    # Combobox-Dest
+    cmb1 = ttk.Combobox(root, width="10", values=(
+        "Pune", "Mumbai", "Delhi", "Chennai"))
+    cmb1.grid(row=2, column=2)
+
+    # Date
+    date_label = Label(root, text="Date")
+    date_label.grid(row=3, column=1)
+    date = Entry(root)
+    date.grid(row=3, column=2)
+    # Submit Button
+    submit = Button(root, text='Show Trains', command=check)
+    submit.grid(row=4, column=1)
+
+    # print(selected_month.get())
+    root.mainloop()
 
 
+def trains10(src, dest):
+    sql = "Select * from trains_info where src='{}' and dest='{}'".format(src, dest)
+    r = Tk()
+    info = c.execute(sql)
+    # print("=============================================================================================")
+    # print("Train No.\t\tTrain Name\t\tSource\t\tSource Time\t\tDestination\t\tDestination Time\t\tFare")
+    # print("=============================================================================================")
+    Label(r, text="Train Number").grid(row=0, column=0)
+    Label(r, text="Train Name").grid(row=0, column=1)
+    Label(r, text="Source").grid(row=0, column=2)
+    Label(r, text="Source Time").grid(row=0, column=3)
+    Label(r, text="Destination").grid(row=0, column=4)
+    Label(r, text="Destination Time").grid(row=0, column=5)
+    Label(r, text="Fare").grid(row=0, column=6)
+    k = 1
+    for i in info:
+        for j in range(len(i)):
+            Label(r, text=i[j]).grid(row=k, column=j)
+        k = k+1
 
-root.mainloop()
+    def goback():
+        #print("Return Button clicked")
+        r.destroy()
+        enter_train_details()
+
+    # Return Back Button
+    backBtn = Button(r, text="Back", command=goback)
+    backBtn.grid(row=k+1, column=0)
+
+
+enter_train_details()
+
 
 
--- trains.py
+++ trains.py
@@ -4,6 +4,7 @@ from tkinter import *
 #root = Tk()
 mydb = sqlite3.connect('trains.db')
 
+
 c= mydb.cursor()
 
 c.execute("""CREATE TABLE IF NOT EXISTS trains_info(
@@ -16,31 +17,16 @@ c.execute("""CREATE TABLE IF NOT EXISTS trains_info(
             fare INT)
             """)
 
-c.execute("""Insert into trains_info values
-    (12890,'Rajdhani Exp','Pune','14:20','Mumbai','17:00',450),
-    (12891,'Sahyadri Exp','Mumbai','14:20','Pune','17:00',80),
-    (17614,'Panvel Festival Special','Pune','6:20','Mumbai','9:00',120)
+#c.execute("""Insert into trains_info values
+#    (12890,'Rajdhani Exp','Pune','14:20','Mumbai','17:00',450),
+#    (12891,'Sahyadri Exp','Mumbai','14:20','Pune','17:00',80),
+#    (17614,'Panvel Festival Special','Pune','6:20','Mumbai','9:00',120)
+#    """)
 
-    """)
+#mydb.commit()
+#db_info = c.execute("SELECT * FROM trains_info")
+#for i in db_info:
+#    print(i)
 
-
-def trains(src,dest):
-    sql = "Select * from trains_info where src='{}' and dest='{}'".format(src,dest)
-    r=Tk()
-    mydb= c.execute(sql)
-    
-    Label(r, text="Train Number").grid(row=0, column=0)
-    Label(r, text="Train Name").grid(row=0, column=1)
-    Label(r, text="Source").grid(row=0, column=2)
-    Label(r, text="Source Time").grid(row=0, column=3)
-    Label(r, text="Destination").grid(row=0, column=4)
-    Label(r, text="Destination Time").grid(row=0, column=5)
-    Label(r, text="Fare").grid(row=0, column=6)
-    k=1
-    for i in mydb:
-        for j in range(len(i)):
-            Label(r, text=i[j]).grid(row=k,column=j)
-        k=k+1
-            
 
    
